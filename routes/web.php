88b38e8ba32018bced49eb91a27a924052e2866c
Martin Sanchez <tinoreny1811@gmail.com>
2022-12-07 07:26:15 -0500

    Mejorando y uniendo codigo en las rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,7 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 //Login
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-
 Route::post('/login', [LoginController::class, 'store']);
-
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 // Dashboard
@@ -39,30 +37,18 @@ Route::get('dashboard', [PostController::class, 'index'])->name('post.index');
 
 // Categories
 Route::get('/categories', [CategoryController::class, 'index'])->name('admin.categories.index');
-
 Route::get('categories/create', [CategoryController::class, 'create'])->name('admin.categories.create');
-
 Route::post('categories', [CategoryController::class, 'store'])->name('admin.categories.store');
-
-Route::get('categories/{category}', [CategoryController::class, 'show'])->name('admin.categories.show');
-
 Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
-
 Route::delete('categories/{category}/destroy', [CategoryController::class, 'destroy'])->name('admin.categories.destroy');
-
 Route::put('categories/{category}', [CategoryController::class, 'update'])->name('admin.categories.update');
 
 // Products
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-
 Route::post('products', [ProductController::class, 'store'])->name('products.store');
-
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
-
 Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-
 Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
 
 
